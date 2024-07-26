f6a8f7db4aa0bcd2b334cf5085f52427afc98453
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-07-26 12:15:14 -0300

    feat: crete user router

---
+++ src/routes/userRouter.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const userRouter = Router();
+
+userRouter.get('/user/profile/:id'); 
+userRouter.post('/signup');
+userRouter.post('/login'); 
+userRouter.put('/user/edit/:id'); 
+userRouter.delete('/user/delete/:id');
