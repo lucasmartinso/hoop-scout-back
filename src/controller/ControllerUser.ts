f894b574e73bead00626c315d85830a7e0110158
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-07-26 14:39:40 -0300

    feat: create route with oop to get all users

--- src/controller/ControllerUser.ts
+++ src/controller/ControllerUser.ts
@@ -1,13 +1,13 @@
 import { getRepository } from "typeorm";
 import { Request, Response} from "express";
 import { Usuario } from "../entity/Usuario";
-import * as UserService from ".."
+//import * as UserService from ".."
 
 export class ControllerUser {
 
     private usuarioRepository = getRepository(Usuario);
 
-    async getAll(req: Request, res: Response) {
+    public async getAll(req: Request, res: Response) {
         try {
             const userList: Usuario[] = await this.usuarioRepository.find();
             return res.status(200).json(userList);
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
+import { ControllerUser } from "../controller/ControllerUser";
 
 const userRouter = Router();
+const controllerUser = new ControllerUser();
 
-userRouter.get('/user/profile/:id'); 
-userRouter.post('/signup');
-userRouter.post('/login'); 
-userRouter.put('/user/edit/:id'); 
-userRouter.delete('/user/delete/:id');
+userRouter.get('/users', controllerUser.getAll.bind(controllerUser));
+//userRouter.get('/user/profile/:id', controllerUser.getAll.bind(controllerUser)); 
+// userRouter.post('/signup');
+// userRouter.post('/login'); 
+// userRouter.put('/user/edit/:id'); 
+// userRouter.delete('/user/delete/:id');
 
 export default userRouter;
