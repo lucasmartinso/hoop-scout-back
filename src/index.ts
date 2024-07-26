e2d52cd4375d4811fd857175b4f1bc992e82f317
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-07-26 14:47:41 -0300

    feat: add package for support routes and async errors

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,36 @@
 import { createConnection } from "typeorm";
 import { Usuario } from "./entity/Usuario";
 
-const express = require('express');
-const body = require('body-parser');
-const port: number = 8080;
+import cors from "cors";
+import dotenv from "dotenv";
+import express, { json } from "express";
+//import "express-async-errors";
+//import routers from "./routers/routers";
+//import errorHandler from "./middlewares/errorHandler";
+dotenv.config();
+
 const app = express();
+app.use(json());
+app.use(cors());
 
-async function start() {
-  createConnection().then(async connection => {
-    const userRepository = connection.getRepository(Usuario);
-  
-    app.get("/", async (req, res) => {
-      const users = await userRepository.find();
-      res.json(users);
-    });
-  
-    app.listen(port, () => {
-      console.log(`Server is running on http://localhost:${port}`);
-    });
-  }).catch(error => console.log(error));
-}
+//app.use(routers);
+//app.use(errorHandler);
 
-start();
+export default app;
+
+// async function start() {
+//   createConnection().then(async connection => {
+//     const userRepository = connection.getRepository(Usuario);
+  
+//     app.get("/", async (req, res) => {
+//       const users = await userRepository.find();
+//       res.json(users);
+//     });
+  
+//     app.listen(port, () => {
+//       console.log(`Server is running on http://localhost:${port}`);
+//     });
+//   }).catch(error => console.log(error));
+// }
+
+// start();
---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+import app from "./index"
+dotenv.config();
+
+const PORT: number = Number(process.env.PORT) || 8080;
+
+app.listen(PORT, () => {
+  console.log(`\nServer is running on http://localhost:${PORT}`);
+});
