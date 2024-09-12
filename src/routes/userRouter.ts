b05ad5c8b4b6f7df5d7a4b4d7d68873d39098be7
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-09-12 16:41:07 -0300

    feat: delete route add

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -12,6 +12,6 @@ userRouter.get('/user/profile', validateTokenAuth,controllerUser.getInfo.bind(co
 userRouter.post('/signup', schemaValidator(userSchema), controllerUser.signup.bind(controllerUser));
 userRouter.post('/login', schemaValidator(loginSchema), controllerUser.login.bind(controllerUser));
 userRouter.put('/user/edit', validateTokenAuth, controllerUser.editProfile.bind(controllerUser)); 
-// userRouter.delete('/user/delete/:id');
+userRouter.delete('/user/delete', validateTokenAuth, controllerUser.deleteProfile.bind(controllerUser));
 
 export default userRouter;
