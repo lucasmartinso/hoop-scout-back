678102c8f3754a32f5c97d8ca85b50452236cc01
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-09-12 11:40:54 -0300

    feat: chaneg route call for get user info

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -9,7 +9,7 @@ const controllerUser = new ControllerUser();
 
 //SO SEGUIR A MESMA ESTRUTURA ABAIXO PARA AS DEMAIS ROTAS DESSE CONTROLLER
 //validateTokenAuth
-userRouter.get('/users', validateTokenAuth,controllerUser.getInfo.bind(controllerUser));
+userRouter.get('/user/profile', validateTokenAuth,controllerUser.getInfo.bind(controllerUser));
 userRouter.post('/signup', schemaValidator(userSchema), controllerUser.signup.bind(controllerUser));
 userRouter.post('/login', schemaValidator(loginSchema), controllerUser.login.bind(controllerUser));
 // userRouter.get('/user/profile/:id', controllerUser.getAll.bind(controllerUser)); 
