6eed5d2ca5f83d581c81385ee669cdd091f7deca
Vitor Leal <vitorlealm.21@gmail.com>
2024-09-14 17:18:24 -0300

    Adiciona tratamento de erros para endpoint de cadastro de pets

--- src/controller/ControllerPet.ts
+++ src/controller/ControllerPet.ts
@@ -4,8 +4,13 @@ import * as petService from "../service/ServicePet";
 
 export class ControllerPet {
     public async createNewPet(req: Request, res: Response) {
+       try {
         const pet: Omit<Pet,'id | createdAt'> = req.body;
         await petService.createPet(pet);
         return res.status(201).send("Pet criado com sucesso");
+       } catch (error) {
+        console.error(error);
+        return res.status(500).send(error.message);
+       }
     }
 }
