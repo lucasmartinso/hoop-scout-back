a1c1bdc8a49344230b55eda7d27acd40c2a49c31
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-09-19 22:05:52 -0300

    feat: create all host routers

--- src/routes/hostRouter.ts
+++ src/routes/hostRouter.ts
@@ -8,7 +8,12 @@ import { ControllerHost } from "../controller/ControllerHost";
 const hostRouter = Router();
 const controllerHost = new ControllerHost();
 
-hostRouter.post('/agendamento', validateTokenAuth,controllerHost.schedule.bind(controllerHost));
-
+hostRouter.post('/hospedagem/agendamento', validateTokenAuth,controllerHost.schedule.bind(controllerHost));
+hostRouter.post('/hospedagem/confirmar', validateTokenAdminAuth, controllerHost.confirmSchedule.bind(controllerHost));
+hostRouter.post('/hospedagem/finalizada', validateTokenAdminAuth, controllerHost.finishHost.bind(controllerHost));
+hostRouter.get('/hospedagem/historico', validateTokenAdminAuth, controllerHost.getHistory.bind(controllerHost));
+hostRouter.get('/hospedagem/orcamento', validateTokenAuth, controllerHost.getPrice.bind(controllerHost));
+hostRouter.put('/hospedagem/editar', validateTokenAuth, controllerHost.editSchedule.bind(controllerHost));
+hostRouter.delete('/hospedagem/cancelar', validateTokenAuth, controllerHost.cancellSchedule.bind(controllerHost));
 
 export default hostRouter;
