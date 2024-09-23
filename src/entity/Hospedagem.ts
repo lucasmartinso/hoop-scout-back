b05e7404f13d6c43301193e59f9dd4c9bfa288cc
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-09-23 10:54:33 -0300

    feat: alter entity host

--- src/entity/Hospedagem.ts
+++ src/entity/Hospedagem.ts
@@ -2,7 +2,7 @@ export class Hospedagem{
     id: number;
     beginDate: Date;
     finishDate: Date | null;
-    status: boolean;
+    status: string | null;
     price: number;
     comment: string;
     createdAt: Date; 
