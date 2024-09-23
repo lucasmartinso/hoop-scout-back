f5058ca1b94afec2036eb0d8c966075d5beb3534
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-09-23 10:53:44 -0300

    feat: alter schema

--- src/schemas/hostSchema.ts
+++ src/schemas/hostSchema.ts
@@ -1,7 +1,8 @@
 import joi, { number } from "joi";
 
 export const hostSchema = joi.object({ 
-    beginDate: joi.date().required().label("Fornecer uma data"),
+    beginDate: joi.date().required().label("Fornecer uma data de inicio"),
+    finishDate: joi.date().required().label("Fornecer uma data de finalizacao"),
     petId: joi.number().integer().required().label("Escolher o pet"),
     comment: joi.string().allow(null).label("Comentario não pode conter somente numeros e simbolos, mas letras, palavras"),
 });
