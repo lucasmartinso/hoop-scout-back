0c874282437872543923db9fa332bd50725d9870
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2025-02-19 18:10:53 -0300

    feat: create repo to coach and athlete

---
+++ src/controller/ControllerAthlete.ts
@@ -0,0 +1,49 @@
+import { Request, Response} from "express";
+import { Users } from "../entity/User";
+import * as userService from "../service/ServiceUser";
+
+export class ControllerUser {
+    public async getInfo(req: Request, res: Response) {
+        const { id }: { id: number } = res.locals.user;
+        const user: Users = await userService.getUserInfo(id);
+
+        return res.status(200).json(user);
+    }
+
+    public async login(req: Request, res: Response) {
+        const user: Omit<Users,'id | createdAt | name'> = req.body;
+
+        const token: string = await userService.login(user);
+
+        return res.status(200).send({token});
+    }
+
+    public async signup(req: Request, res: Response) {
+        const user: Omit<Users,'id | createdAt'> = req.body;
+
+        await userService.signup(user);
+
+        return res.status(201).send("Created user sucess");
+    }
+
+    public async editProfile(req: Request, res: Response) {
+        const { id }: { id: number } = res.locals.user;
+        const user: Omit<Users,'id | createdAt'> = req.body;
+
+        await userService.editProfile(user, id);
+
+        return res.status(200).send("Edited user profile sucess");
+    }
+
+    public async verifyAuthUser(req: Request, res: Response) {
+        return res.status(200).send("Perfil com autorizacao atleta");
+    }
+
+    public async verifyAuthAthlete(req: Request, res: Response) {
+        return res.status(200).send("Perfil com autorizacao de treinador");
+    }
+
+    public async verifyAuthCoach(req: Request, res: Response) {
+        return res.status(200).send("Perfil com autorizacao de treinador");
+    }
+}
