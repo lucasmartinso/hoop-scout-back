ea95a58ecb0f55e89237e9f625c6aad609da5778
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2025-02-22 00:20:43 -0300

    feat: rating schema at route

--- src/schemas/ratingSchema.ts
+++ src/schemas/ratingSchema.ts
@@ -1,6 +1,6 @@
 import joi from "joi";
 
-export const userSchema = joi.object({ 
+export const ratingSchema = joi.object({ 
     age: joi.number().max(18).required().label("Atleta tem que ter ate 18 anos"),
     height: joi.string().length(5).required().label("Altura tem que ser no formato X.XXm"),
     weight: joi.string().min(7).max(8).required().label("Peso tem que ser no formato XX.XXkg ou XXX.XXkg"),
