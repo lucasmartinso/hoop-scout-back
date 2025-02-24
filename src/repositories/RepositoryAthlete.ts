4043cbe1e6a9f6eeeb5244271503818881433e83
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2025-02-24 15:37:21 -0300

    feat: repo for athlete is done

--- src/repositories/RepositoryAthlete.ts
+++ src/repositories/RepositoryAthlete.ts
@@ -1,1 +1,23 @@
+import { QueryResult } from "pg";
+import connection from "../database/postgres";
 import { Athlete } from "../entity/Athlete";
+
+export async function getProbabilityCalc(id: number): Promise<Athlete[]> {
+    const { rows: athletes }: QueryResult<Athlete> = await connection.query(`
+        SELECT "height", "weight", "age", "freeThrow", "longShot", "shortShot", "assistsGame"
+        FROM "Athlete"
+        WHERE "id" = $1;
+    `);
+    
+    return athletes;
+}
+
+export async function getAthleteData(id: number): Promise<Athlete[]> {
+    const { rows: athletes }: QueryResult<Athlete> = await connection.query(`
+        SELECT * 
+        FROM "Athlete" 
+        WHERE "id" = $1;
+    `,[id]);
+    
+    return athletes;
+}
