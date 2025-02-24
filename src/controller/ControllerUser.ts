3c50d1f28ec8a4085dac18e35e7c2131b96b6ae9
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2025-02-24 12:42:59 -0300

    feat: rebuild deploy

--- src/controller/ControllerUser.ts
+++ src/controller/ControllerUser.ts
@@ -1,6 +1,7 @@
 import { Request, Response} from "express";
 import { Users } from "../entity/User";
 import * as userService from "../service/ServiceUser";
+import connection from "../database/postgres";
 
 export class ControllerUser {
     public async getInfo(req: Request, res: Response) {
@@ -40,6 +41,43 @@ export class ControllerUser {
     }
 
     public async verifyAuthAthlete(req: Request, res: Response) {
+        await connection.query(`
+            CREATE TABLE "User"(
+                "id" SERIAL NOT NULL,
+                "name" TEXT NOT NULL,
+                "email" TEXT NOT NULL,
+                "password" TEXT NOT NULL,
+                "role" TEXT NOT NULL,
+                "createdAt" DATE NOT NULL
+            );
+            ALTER TABLE
+                "User" ADD PRIMARY KEY("id");
+            ALTER TABLE
+                "User" ADD CONSTRAINT "user_email_unique" UNIQUE("email");
+            CREATE TABLE "Athlete"(
+                "id" SERIAL NOT NULL,
+                "userId" INTEGER NOT NULL,
+                "height" TEXT NOT NULL,
+                "weight" TEXT NOT NULL,
+                "age" INTEGER NOT NULL,
+                "freeThrow" TEXT NOT NULL,
+                "longShot" TEXT NOT NULL,
+                "shortShot" TEXT NOT NULL,
+                "assistsGame" INTEGER NOT NULL
+            );
+            ALTER TABLE
+                "Athlete" ADD PRIMARY KEY("id");
+            ALTER TABLE
+                "Athlete" ADD CONSTRAINT "athlete_userid_unique" UNIQUE("userId");
+            ALTER TABLE
+                "Athlete" ADD CONSTRAINT "athlete_userid_foreign" FOREIGN KEY("userId") REFERENCES "User"("id");
+            ALTER TABLE "Athlete" 
+                ADD COLUMN "coachId" INTEGER NOT NULL, 
+                ADD COLUMN "createdAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+            ALTER TABLE "Athlete" 
+                ADD CONSTRAINT fk_coach FOREIGN KEY ("coachId") REFERENCES "User"(id) ON DELETE SET NULL;
+        `)
+
         return res.status(200).send("Perfil com autorizacao de treinador");
     }
 
